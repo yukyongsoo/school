23959da04b78707423be477dca46214f4cc76479
yys <yozx234@gmail.com>
2022-09-26 01:46:12 +0900

    add swagger
    logging query while test

--- src/main/kotlin/com/yuk/school/SchoolApplication.kt
+++ src/main/kotlin/com/yuk/school/SchoolApplication.kt
@@ -1,12 +1,19 @@
 package com.yuk.school
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.OpenAPI
+import org.springdoc.core.GroupedOpenApi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+import org.springframework.context.annotation.Bean
+
 
 @SpringBootApplication
+@OpenAPIDefinition(info = Info(title = "School Demo", version = "1.0", description = "Documentation APIs v1.0"))
 class SchoolApplication
 
 fun main(args: Array<String>) {
 	runApplication<SchoolApplication>(*args)
 }
+
