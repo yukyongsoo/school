9ed99f944df763ad540512a6de276ac2d4bb6afa
yys <yozx234@gmail.com>
2022-09-28 01:00:19 +0900

    add StudentService

---
+++ src/test/kotlin/com/yuk/school/student/StudentServiceTest.kt
@@ -0,0 +1,41 @@
+package com.yuk.school.student
+
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
+
+@ExtendWith(MockitoExtension::class)
+class StudentServiceTest {
+    @Mock
+    private lateinit var studentRepository: StudentRepository
+
+    @InjectMocks
+    private lateinit var studentService: StudentService
+
+    @Test
+    fun `학생 신규 등록`() {
+        runBlocking {
+            given(studentRepository.save(any())).willReturn(
+                Student("학생")
+            )
+
+            studentService.save()
+        }
+    }
+
+    @Test
+    fun `학생 조회`() {
+        runBlocking {
+            given(studentRepository.getOrNull(any())).willReturn(
+                Student("학생")
+            )
+
+            studentService.get(StudentId("507f1f77bcf86cd799439011"))
+        }
+    }
+}
