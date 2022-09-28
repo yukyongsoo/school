1e79119360c6e50cdffe3f52c36da097ff1175d4
yukyongsoo <yozx234@gmail.com>
2022-09-28 18:20:50 +0900

    add Subject Handler

---
+++ src/test/kotlin/com/yuk/school/subject/inbound/SubjectHandlerTest.kt
@@ -0,0 +1,50 @@
+package com.yuk.school.subject.inbound
+
+import com.yuk.school.subject.Subject
+import com.yuk.school.subject.SubjectService
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.given
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+
+@WebFluxTest(SubjectHandler::class)
+class SubjectHandlerTest {
+    @Autowired
+    private lateinit var webTestClient: WebTestClient
+
+    @MockBean
+    private lateinit var subjectService: SubjectService
+
+    @Test
+    fun `과목 생성`() {
+        runBlocking {
+            given(subjectService.save()).willReturn(
+                Subject("과목")
+            )
+        }
+
+        webTestClient.post().uri("/subject")
+            .exchange()
+            .expectStatus().isOk
+    }
+
+    @Test
+    fun `과목 조회`() {
+        runBlocking {
+            given(subjectService.get(any())).willReturn(
+                Subject("과목")
+            )
+        }
+
+        webTestClient.get()
+            .uri("/subject/{id}", "507f1f77bcf86cd799439011")
+            .exchange()
+            .expectStatus().isOk
+            .expectBody<Subject>()
+    }
+}
