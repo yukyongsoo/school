50fcb64ded55c34385dbf060970bfdf4d60146f0
yukyongsoo <yozx234@gmail.com>
2022-09-28 12:23:46 +0900

    wip

--- src/main/kotlin/com/yuk/school/student/Student.kt
+++ src/main/kotlin/com/yuk/school/student/Student.kt
@@ -3,7 +3,9 @@ package com.yuk.school.student
 import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.Transient
+import org.springframework.data.mongodb.core.mapping.Document
 
+@Document
 class Student(
     val name: String
 ) {
