03c0c3f5665fd6f40635ea0d6049192f33cae503
yys <yozx234@gmail.com>
2022-10-04 01:15:37 +0900

    make lesson comparable timeTable lessons type changed to sortedSet

--- src/main/kotlin/com/yuk/school/classroom/TimeTable.kt
+++ src/main/kotlin/com/yuk/school/classroom/TimeTable.kt
@@ -2,6 +2,7 @@ package com.yuk.school.classroom
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 
 @Document
 class TimeTable(
@@ -14,6 +15,9 @@ class TimeTable(
     @Id
     private var _id = classRoomId.toObjectId()
 
-    var lessons = listOf<Lesson>()
-        private set
+    @Field("lesson")
+    private var _lessons = sortedSetOf<Lesson>()
+
+    val lesson: Set<Lesson>
+        get() = _lessons
 }
