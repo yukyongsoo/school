58ae448f9bf0e11f012bbf774fef8e4299314466
yys <yozx234@gmail.com>
2022-10-06 01:32:59 +0900

    add TimeTableService

---
+++ src/main/kotlin/com/yuk/school/classroom/TimeTableService.kt
@@ -0,0 +1,27 @@
+package com.yuk.school.classroom
+
+import org.springframework.stereotype.Service
+
+@Service
+class TimeTableService(
+    private val timeTableRepository: TimeTableRepository
+) {
+    suspend fun save(classRoomId: ClassRoomId): TimeTable {
+        if (timeTableRepository.getOrNull(classRoomId) != null)
+            throw IllegalStateException()
+
+        return timeTableRepository.save(TimeTable.empty(classRoomId))
+            ?: throw RuntimeException()
+    }
+
+    suspend fun get(classRoomId: ClassRoomId): TimeTable {
+        return timeTableRepository.getOrNull(classRoomId)
+            ?: throw RuntimeException()
+    }
+
+    suspend fun addLesson(classRoomId: ClassRoomId, newLesson: Lesson) {
+        val timeTable = get(classRoomId)
+        timeTable.addLesson(newLesson)
+        timeTableRepository.save(timeTable)
+    }
+}
