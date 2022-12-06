dd0543fd3cccb13b603ae4e493f51f540c323bd1
yys <yozx234@gmail.com>
2022-12-07 02:41:52 +0900

    subject arch test and refactoring

--- src/main/kotlin/com/yuk/school/subject/outbound/SubjectMongoRepository.kt
+++ src/main/kotlin/com/yuk/school/subject/outbound/SubjectMongoRepository.kt
@@ -1,7 +1,18 @@
 package com.yuk.school.subject.outbound
 
 import com.yuk.school.subject.Subject
+import com.yuk.school.subject.SubjectId
+import com.yuk.school.subject.SubjectRepository
+import kotlinx.coroutines.reactor.awaitSingleOrNull
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 
-interface SubjectMongoRepository : ReactiveMongoRepository<Subject, ObjectId>
+interface SubjectMongoRepository : ReactiveMongoRepository<Subject, ObjectId>, SubjectRepository {
+    override suspend fun new(entity: Subject): Subject? {
+        return save(entity).awaitSingleOrNull()
+    }
+
+    override suspend fun get(subjectId: SubjectId): Subject? {
+        return findById(subjectId.toObjectId()).awaitSingleOrNull()
+    }
+}
